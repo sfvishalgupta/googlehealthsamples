fe5579d39a196859ee186209f8065a81ffc300ef
nyx@google.com <nyx@google.com@ded4488a-e653-0410-a436-17cba34053d5>
2010-12-21 10:08:15 +0000

    Removed unnecessary import.

--- trunk/java/appengine/src/com/google/health/examples/appengine/oauth/OAuthFilter.java
+++ trunk/java/appengine/src/com/google/health/examples/appengine/oauth/OAuthFilter.java
@@ -11,8 +11,6 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import sun.util.logging.resources.logging;
-
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
